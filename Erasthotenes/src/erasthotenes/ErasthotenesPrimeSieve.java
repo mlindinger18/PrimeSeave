700d9f995e99e62a96009c1eb0bf081397786f48
Marcel Lindinger <mlindinger18@gmail.com>
2021-09-21 14:46:00 +0200

    PrimeSieve implemented

--- Erasthotenes/src/erasthotenes/ErasthotenesPrimeSieve.java
+++ Erasthotenes/src/erasthotenes/ErasthotenesPrimeSieve.java
@@ -9,6 +9,22 @@ package erasthotenes;
  *
  * @author Marcel.Lindinger
  */
-public class ErasthotenesPrimeSieve {
+public class ErasthotenesPrimeSieve implements PrimeSieve {
+
+    public final int maximum;
+
+    public ErasthotenesPrimeSieve() {
+        maximum = 30;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void printPrimes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
