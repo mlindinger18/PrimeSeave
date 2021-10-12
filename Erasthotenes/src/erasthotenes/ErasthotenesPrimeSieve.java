9c48e388f0b8e553a964cbcfe46972351168c964
Marcel Lindinger <mlindinger18@gmail.com>
2021-10-12 14:54:32 +0200

    Aufgabe fertig

--- Erasthotenes/src/erasthotenes/ErasthotenesPrimeSieve.java
+++ Erasthotenes/src/erasthotenes/ErasthotenesPrimeSieve.java
@@ -5,36 +5,67 @@
  */
 package erasthotenes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Marcel.Lindinger
  */
 public class ErasthotenesPrimeSieve implements PrimeSieve {
 
-    public ErasthotenesPrimeSieve() {
+    public int number;
+    public int og;
+    List<Integer> primes = new ArrayList<>();
 
+    public ErasthotenesPrimeSieve(int number, int og) {
+        this.number = number;
+        this.og = og;
     }
 
     @Override
-    public boolean isPrime(int p) {
-        boolean prime[] = new boolean[p + 1];
-        for (int i = 0; i <= p; i++) {
-            prime[i] = true;
-        }
-        for (int n = 2; n * n <= p; n++) {
-            if (prime[p] == true) {
+    public boolean isPrime(int og) {
 
-            }
-            for (int i = p * p; i <= n; i += p) {
-                prime[i] = false;
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
             }
         }
-        return false;
+        return true;
+
+    }
+
+    public void PrimesAdd(int o) {
+        ErasthotenesPrimeSieve eps = new ErasthotenesPrimeSieve(number, og);
+        int n = 0;
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < o; i++) {
+            if (eps.isPrime(i) == true) {
+                x = i;
+            }
+
+            for (int j = 10; j <= o; j--) {
+                if (eps.isPrime(i) == true) {
+                    y = j;
+                }
+                n = x + y;
+                if (eps.isPrime(n) == true) {
+                    System.out.println(n);
+                }
+            }
+
+        }
+
     }
 
     @Override
     public void printPrimes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Folgende Zahlen sind Primzahlen: ");
+        System.out.println("... " + ErasthotenesPrimeSieve.this.isPrime(100));
     }
 
 }
