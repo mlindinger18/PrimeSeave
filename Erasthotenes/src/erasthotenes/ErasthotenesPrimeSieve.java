e9cdf6a0f8c952dc70774b1d7a605768a4034af2
mlindinger18 <71641439+mlindinger18@users.noreply.github.com>
2022-02-01 09:01:27 +0100

    final version

--- Erasthotenes/src/erasthotenes/ErasthotenesPrimeSieve.java
+++ Erasthotenes/src/erasthotenes/ErasthotenesPrimeSieve.java
@@ -7,6 +7,7 @@ package erasthotenes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -14,63 +15,85 @@ import java.util.List;
  */
 public class ErasthotenesPrimeSieve implements PrimeSieve {
 
-    public int number;
-    public int og;
+    private int maxHeight;
+    private boolean[] array;
 
-    public ErasthotenesPrimeSieve(int number, int og) {
-        this.number = number;
-        this.og = og;
+    public ErasthotenesPrimeSieve(int maxHoehe) {
+        this.maxHeight = maxHeight;
+        array = new boolean[maxHeight];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = true;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Geben sie die maximale Höhe ein:");
+        int height = Integer.parseInt(sc.nextLine());
+        ErasthotenesPrimeSieve eps = new ErasthotenesPrimeSieve(height);
+        eps.array[0] = false;
+        eps.array[1] = false;
+        eps.array[2] = true;
+        for (int i = 2; i < eps.array.length; i++) {
+            for (int j = 2; j < eps.array.length && eps.array[i] == true && i * j < eps.array.length; j++) {
+                eps.array[i * j] = false;
+            }
+        }
+
+        eps.printPrimes();
+
+        System.out.println("");
+
+        eps.findSum(eps.array);
     }
 
     @Override
-    public boolean isPrime(int og) {
-
-        if (number <= 1) {
-            return false;
+    public boolean isPrime(int p) {
+        if (array[p] == true) {
+            return true;
         }
-        for (int i = 2; i < og; i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-        return true;
-
-    }
-
-    public void PrimesAdd(int o) {
-        ErasthotenesPrimeSieve eps = new ErasthotenesPrimeSieve(number, og);
-        int n = 0;
-        int x = 0;
-        int y = 0;
-        for (int i = 0; i < o; i++) {
-            if (eps.isPrime(i) == true) {
-                x = i;
-            }
-
-            for (int j = 10; j <= o; j--) {
-                if (eps.isPrime(j) == true) {
-                    y = j;
-                }
-                n = x + y;
-                if (n % 2 == 0) {
-                    System.out.println(n + "= " + x + "+ " + y);
-                }
-            }
-
-        }
+        return false;
 
     }
 
     @Override
     public void printPrimes() {
-        List<Integer> primes = new ArrayList<>();
-        for (int i = 2; i < og; i++) {
-            if (ErasthotenesPrimeSieve.this.isPrime(i) == true) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == true) {
+                System.out.print(i + ",");
+            }
+        }
+    }
+
+    public void findSum(boolean[] primesBoolean) {
+        ArrayList<Integer> primes = new ArrayList<>();
+        ArrayList<String> erg = new ArrayList<>();
+
+        for (int i = 0; i < primesBoolean.length; i++) {
+            if (primesBoolean[i] == true) {
                 primes.add(i);
             }
+        }
+
+        boolean b = true;
+        for (int i = 2; i < primesBoolean.length; i += 2) {
+            b = true;
+            for (int j = 0; j < primes.size() && b == true; j++) {
+
+                for (int k = 0; k < primes.size() && b == true; k++) {
+                    if (i == primes.get(j) + primes.get(k)) {
+                        b = false;
+                        int ergebnis = primes.get(j) + primes.get(k);
+                        erg.add(ergebnis + " summe: " + ergebnis + " = " + primes.get(j) + " + " + primes.get(k));
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < erg.size(); i++) {
+            System.out.println(erg.get(i));
 
         }
-        System.out.println(primes);
     }
 
 }
